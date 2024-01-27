0c96056a0c9747ad50b86d0b78a9d208e07ed22b
Joseph Leung <leung.joseph@outlook.com>
2024-01-27 00:57:19 -0500

    Update historicalEvents.ts

--- server/src/historicalEvents.ts
+++ server/src/historicalEvents.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 // variables used for request
 const year = 2020; // placeholder, needs to be changed
-const currentYear = new Date().getFullYear();
+
 const apiKey = 'HtRf0xMWbc/x6VphtgdRGg==FEkcU81DXwIAe5hM';
 
 // call request
@@ -18,14 +18,14 @@ axios.get(`https://api.api-ninjas.com/v1/historicalevents?year=${year}`, {
     console.log('Response:', response.data);
 
     // picking random entry from the year, returning object for given index
-    function genRandYear(selectedYear:number, curYear:number):number { // returns a random year in between the 2 years provided
+    function genRandNum(min:number, max:number):number { // returns 
       const randomFloat = Math.random();
-      const randomNumber = Math.floor(randomFloat * (curYear - selectedYear + 1)) + selectedYear;
+      const randomNumber = Math.floor(randomFloat * (max - min + 1)) + min;
       return randomNumber;
     };
 
-    const randYear = genRandYear(year, currentYear);
-    const entry = response.data[randYear];
+    const randNum = genRandNum(0, response.data.length - 1);
+    const entry = response.data[randNum];
 
     // entry information [day, month, event description]
     const entryInfo = [entry.day, entry.month, entry.event];
