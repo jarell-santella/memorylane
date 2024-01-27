39ba0fc70374ca841d889d8a1949dc30795fb4d4
js <50718474+jarell-santella@users.noreply.github.com>
2024-01-27 18:15:16 -0500

    add: boilerplate sections for events, movies, and tv shows

---
+++ client/src/components/EventsSection.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const EventsSection = ({ year }: { year: string }) => {
+  const [data, setData] = useState<any>(null)
+  const [error, setError] = useState<any>(null)
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/get/events/${year}`)
+      .then((response) => {
+        setData(response.data) // Data is available in the `data` property of the response
+        console.log(year)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error)
+        setError(error)
+      })
+  }, [year])
+
+  if (error) return <div>Error loading data</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <>
+      {Object.keys(data).map((yearKey) => (
+        <div key={yearKey}>
+          <h3>Year: {yearKey}</h3>
+          <p>Event Date: {data[yearKey].eventDate}</p>
+          <p>Event Name: {data[yearKey].eventName}</p>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default EventsSection
